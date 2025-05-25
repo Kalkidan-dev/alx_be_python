09a2900ec8cadd2d7a18c2a1dee7746f24c16705
KalDev-2 <kalkidangemechudev@gmail.com>
2025-05-25 20:49:00 +0300

    Create a script

--- basic_operations.py
+++ basic_operations.py
@@ -1,13 +1,12 @@
-# Assign values to variables
 number1 = 10
 number2 = 5
 
-# Perform operations
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
 
 # Print results
-print(f"Addition of {number1} and {number2} is {addition}")
-print(f"Subtraction of {number1} and {number2} is {subtraction}")
-print(f"Multiplication of {number1} and {number2} is {multiplication}")
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
